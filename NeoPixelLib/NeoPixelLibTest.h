607350cb57d4c7dfc8ab20c535469f5ec1be292c
The Guru <cyberreefguru@gmail.com>
2015-09-15 18:30:03 -0400

    Create new wrapper library using FastLED

---
+++ NeoPixelLib/NeoPixelLibTest.h
@@ -0,0 +1,14 @@
+/*
+ * NeoPixelLibTest.h
+ *
+ *  Created on: Sep 12, 2015
+ *      Author: tsasala
+ */
+
+#ifndef NEOPIXELLIBTEST_H_
+#define NEOPIXELLIBTEST_H_
+
+#include "NeoPixelWrapper.h"
+
+
+#endif /* NEOPIXELLIBTEST_H_ */
